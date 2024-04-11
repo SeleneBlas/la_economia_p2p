f36619aeb209e1f6aeb077e1d6a2320121ea91a3
Selene <sele.blas02@gmail.com>
2024-04-11 14:42:02 -0300

    actualizacion

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './script.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new webpack.EnvironmentPlugin(['CLIENT_SECRET'])
+    ]
 };
