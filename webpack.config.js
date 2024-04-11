de8b79731d19b75f926a1ae1697a845dba646905
Selene <sele.blas02@gmail.com>
2024-04-11 11:38:04 -0300

    actualizacion

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: 'script.js',
+    entry: './script.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
