3bdef8191c99469473121f0047e8a9734bb2cc27
Selene <109917974+SeleneBlas@users.noreply.github.com>
2024-06-13 10:33:32 -0300

    Update webpack.config.js
    Signed-off-by: Selene <109917974+SeleneBlas@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: '../scripts.js',
+  entry: './scripts.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
